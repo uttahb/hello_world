cd30743c85f9e67ca1b38a6c368457b3b506644b
Jishnu <jishnunbhattathiri@gmail.com>
2022-10-03 06:40:33 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const hello_world = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello hello_world`}))
+  res.end()
+  
+}
+
+export default hello_world
